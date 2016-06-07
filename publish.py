f837a58828ba3d1608d1fe9b0e6b725d5a05e15f
fserena <kudhmud@gmail.com>
2016-06-07 07:45:22 +0000

    Fixed publish script

--- publish.py
+++ publish.py
@@ -5,15 +5,17 @@ import config
 
 exchange = config.AMQP_EXCHANGE
 
-connection_params = pika.ConnectionParameters(
-    host=config.AMQP_HOST, port=config.AMQP_PORT,
-    credentials=pika.credentials.PlainCredentials(config.AMQP_USER, config.AMQP_PASS)
-)
-connection = pika.BlockingConnection(connection_params)
-
-ch = connection.channel()
-
 
 def publish(data):
-    ch.basic_publish(exchange=exchange, routing_key='annotations.consensus', body=json.dumps(data))
-    print '{} -> |consensus|'.format(data)
+	connection_params = pika.ConnectionParameters(
+	    host=config.AMQP_HOST, port=config.AMQP_PORT,
+	    credentials=pika.credentials.PlainCredentials(config.AMQP_USER, config.AMQP_PASS)
+	)
+	connection = pika.BlockingConnection(connection_params)
+
+	ch = connection.channel()
+
+    	ch.basic_publish(exchange=exchange, routing_key='annotations.consensus', body=json.dumps(data))
+    	print '{} -> |consensus|'.format(data)
+
+	connection.close()
